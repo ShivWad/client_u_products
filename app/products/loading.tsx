73f2429c9d5f43a8f9d2385a364ac417619715d4
ShivWad <shivamwadhawan2000@gmail.com>
2023-11-07 18:21:13 +0530

    categories, proxy route and more

---
+++ app/products/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <h1>Load hone de be</h1>
+  )
+}
+
+export default loading
---
+++ app/products/page.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+type USER = {
+    "_id": string,
+    "name": string,
+    "email": string,
+    "password": string,
+    "createdAt": Date,
+    "updatedAt": Date,
+    "__v": number
+}
+
+const handleGetAllUser = async (): Promise<USER> => {
+    let res = await fetch("http://localhost:3000/api/user");
+
+
+
+    let responseJson = await res.json();
+    console.log(responseJson);
+    return responseJson;
+}
+
+const page = async () => {
+
+    let users = await handleGetAllUser();
+    return (
+        <div>
+            <h1>{users.email}</h1>
+            <h1>{users.name}</h1>
+            <h1>{users._id}</h1>
+        </div>
+    )
+}
+
+export default page
